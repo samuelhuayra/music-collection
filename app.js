99a5e2a150d869023443e7509b1e47f6066bd175
Samuel Huayra <samuelhuayra@gmail.com>
2020-03-14 12:25:05 -0400

    backend structure

---
+++ app.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+const express = require('express')
+const path = require('path')
+const fs = require('fs')
+const morgan = require('morgan')
+const createError = require('http-errors')
+const cors = require('cors')
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODBCONECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const app = express()
+app.use(cors())
+app.options('*', cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(morgan('combined', { stream: fs.createWriteStream('app.log', { flags: 'a' }) }))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')))
+
+app.use((req, res, next) => {
+  next(createError(404))
+})
+app.use((err, req, res, next) => {
+  next(createError(400, err))
+})
+
+module.exports = app
---
+++ bin/www.js
@@ -0,0 +1,12 @@
+require('dotenv').config()
+const app = require('../app')
+const http = require('http')
+
+const PORT = process.env.PORT || 5000
+app.set('port', PORT)
+
+const server = http.createServer(app)
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
